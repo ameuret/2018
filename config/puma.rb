949c80a178366b10d644f7077db96a13569a10d2
Arnaud Meuret <arnaud@meuret.net>
2023-07-24 17:47:15 +0900

    Be more friendly forgetful devs 😀👈

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
 require 'dotenv/load'
-raise "The SN environment variable should be set. Edit .env file or export it from parent process." unless ENV['SN']
-raise "The PUMA_BIND environment variable must be set. Edit .env file or export it from parent process." unless ENV['PUMA_BIND']
+raise "The SN environment variable should be set. Edit .env file or export it from parent process.\ne.g. in .env: SN='GTS'" unless ENV['SN']
+raise "The PUMA_BIND environment variable must be set. Edit .env file or export it from parent process.\ncf. https://github.com/puma/puma#binding-tcp--sockets" unless ENV['PUMA_BIND']
 bind ENV['PUMA_BIND']
